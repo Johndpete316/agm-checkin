ab7dc932a09653e0b1bb6bc3099dd5f98f945013
John Peterson <johndpete5316@outlook.com>
2026-07-28 21:02:32 -0400

    fix: update allowed origin and database port in configuration files

--- agm-checkin-api/dev.fish
+++ agm-checkin-api/dev.fish
@@ -2,5 +2,5 @@
 
 set -x DATABASE_URL (grep DATABASE_URL .env | cut -d= -f2-)
 set -x AUTH_PIN 1234
-set -x ALLOWED_ORIGIN http://localhost:5173
+set -x ALLOWED_ORIGIN http://10.16.10.236:5173
 go run ./bin/api
